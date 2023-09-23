960ce9104b1de94164921def2824009e3c9429c9
metalfurius <hackero1932@gmail.com>
2023-09-23 20:29:02 +0200

    Update ActMoveSpriteDown.cs

--- Assets/Game/Runtime/Visual Novel System/Configs/ActTypes/ActMoveSpriteDown.cs
+++ Assets/Game/Runtime/Visual Novel System/Configs/ActTypes/ActMoveSpriteDown.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
+using System.Collections;
 
 [CreateAssetMenu(menuName = "Visual Novel/Acts/Move Sprite Down", fileName = "New Act Move Sprite Down")]
 public class ActMoveSpriteDown : ActConfig
 {
     public float moveDistance = 1.0f; // Distancia a mover hacia abajo.
+    public float moveDuration = 1.0f; // Duración del movimiento en segundos.
+
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+    private float startTime;
 
     public override void OnStart()
     {
         Debug.Log("Start Move Sprite Down Act");
-        // Agrega el código para mover el Sprite aquí.
+
         if (Narrator.Instance.spriteGameObject != null)
         {
-            Vector3 newPosition = Narrator.Instance.spriteGameObject.transform.position - Vector3.up * moveDistance;
-            Narrator.Instance.spriteGameObject.transform.position = newPosition;
+            initialPosition = Narrator.Instance.spriteGameObject.transform.position;
+            targetPosition = initialPosition - Vector3.up * moveDistance;
+            startTime = Time.time;
+
+            // Inicia la corutina para realizar el movimiento gradual.
+            Narrator.Instance.StartCoroutine(MoveSprite());
         }
-        // Llama a NextAct para continuar la narración.
+    }
+
+    private IEnumerator MoveSprite()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            elapsedTime = Time.time - startTime;
+            float t = Mathf.Clamp01(elapsedTime / moveDuration);
+            Vector3 newPosition = Vector3.Lerp(initialPosition, targetPosition, t);
+            Narrator.Instance.spriteGameObject.transform.position = newPosition;
+            yield return null;
+        }
+
+        // Cuando el movimiento esté completo, llamamos a NextAct para continuar la narración.
         Narrator.Instance.NextAct();
     }
 
